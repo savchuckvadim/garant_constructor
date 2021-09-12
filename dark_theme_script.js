012e9cf256b4e4bcf21a5ac6543bff24db1e85c8
savchuckvadim <savchuckvadim@gmail.com>
2021-09-12 22:43:42 +0300

    fixed darkTheme

--- dark_theme_script.js
+++ dark_theme_script.js
@@ -2,6 +2,11 @@ const color = document.getElementById('color');
 const growing = document.getElementById('growing');
 const round = document.getElementById('round');
 const body = document.querySelector('#body');
+const header = document.querySelector('#header')
+const btn = document.querySelectorAll('.btns__complect')
+const exampleModal = document.querySelector('#modal-dialog')
+const result = document.querySelector('#result')
+
 var flag = false;
 
 // color.addEventListener('click', function(){
@@ -39,19 +44,39 @@ round.addEventListener('click', () => {
         round.style.backgroundColor = "white";
         body.style.backgroundColor = "white";
         body.style.color = "black";
-        round.style.backgroundImage = "url('./img/darktheme/moon-fill.svg')";
+
+        exampleModal.style.backgroundColor = "white";
+        exampleModal.style.color = "black";
+        result.style.backgroundColor = "white";
+        result.style.color = "black";
+        btn.forEach((element) => {
+            element.style.color = "black"; 
+        })
+
+        header.style.backgroundColor = "white";
+        // round.style.backgroundImage = "url('./img/darktheme/moon-fill.svg')";
+        round.style.backgroundImage = "url('./img/broSunMoon/moon.svg')"
         round.style.transform = "rotateZ(3600deg)";
       
         return flag = false;
         
     }else{
-        body.style.backgroundColor = "black";
-        body.style.color = "rgb(152, 224, 224)";
+        body.style.backgroundColor = "rgba(37, 40, 45, 1)";
+        body.style.color = "white"; 
+        btn.forEach((element) => {
+            element.style.color = "white"; 
+        })
+        exampleModal.style.backgroundColor = "rgba(37, 40, 45, 1)";
+        exampleModal.style.color = "white";
+        result.style.backgroundColor = "rgba(37, 40, 45, 1)";
+        result.style.color = "white";
+        result.style.borderStyle = 'doted'
+        header.style.backgroundColor = "rgba(37, 40, 45, 1)";
         
 
-        round.style.backgroundColor = "yellow";
+        round.style.backgroundColor = "";
 
-        round.style.backgroundImage = "url('./img/darktheme/sun.svg')";
+        round.style.backgroundImage = "url('./img/broSunMoon/sun.svg')";
         round.style.transform = "rotateZ(-3600deg)";
         
         
